b690a119bd96e201eeb0fbd851fdf5deee1f6607
Atanas Atanasov <scriptex.bg@gmail.com>
2018-02-23 13:24:18 +0200

    Add first version of script

---
+++ index.js
@@ -0,0 +1,26 @@
+export const debounce = (func, wait, immediate) => {
+	let timeout;
+
+	wait = wait ? wait : 15;
+
+	return () => {
+		const context = this;
+		const args = arguments;
+		const later = () => {
+			timeout = null;
+
+			if (!immediate) {
+				func.apply(context, args);
+			}
+		};
+		const callNow = immediate && timeout;
+
+		clearTimeout(timeout);
+
+		timeout = setTimeout(later, wait);
+
+		if (callNow) {
+			func.apply(context, args);
+		}
+	};
+};
