79a893c719d1d7d7496c5f1e338ac85b0a87e536
Atanas Atanasov <scriptex.bg@gmail.com>
2022-11-24 15:04:08 +0200

    Remove demo folder

--- demo/debounce.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const debounce: <T extends (...args: unknown[]) => void>(callback: T, wait?: number, immediate?: boolean) => <C>(this: C, ...args: Parameters<T>) => void;
-export default debounce;
